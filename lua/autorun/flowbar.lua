bbce2a85012f1e17697fbbd872a6882ce719dd5b
MysteryPancake <MysteryPancake@users.noreply.github.com>
2017-10-21 21:47:07 +1100

    Extra tab (wow)

--- lua/autorun/flowbar.lua
+++ lua/autorun/flowbar.lua
@@ -1,6 +1,7 @@
 hook.Add( "DoAnimationEvent", "Flowbar", function( ply )
 
 	local vm = ply:GetViewModel()
+
 	if not string.find( vm:GetModel(), "crowbar" ) then return end
 
 	local seq = vm:GetSequence()
