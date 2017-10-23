5c492be18d8eeef33cc5eadc7fa98b88bd9ee270
MysteryPancake <MysteryPancake@users.noreply.github.com>
2017-10-23 16:17:54 +1100

    Removed tab (WOW)

--- lua/autorun/flowbar.lua
+++ lua/autorun/flowbar.lua
@@ -6,7 +6,6 @@ hook.Add( "DoAnimationEvent", "Flowbar", function( ply )
 
 	local seq = vm:GetSequence()
 	local act = vm:GetSequenceActivity( seq )
-
 	if act == ACT_VM_HITCENTER then
 		local miss = vm:SelectWeightedSequence( ACT_VM_MISSCENTER )
 		vm:SendViewModelMatchingSequence( miss )
