f840629ac693b243da0de927821d650fbfbb2b30
MysteryPancake <MysteryPancake@users.noreply.github.com>
2017-10-25 18:56:02 +1100

    Model list (maybe update)

--- lua/autorun/flowbar.lua
+++ lua/autorun/flowbar.lua
@@ -1,8 +1,22 @@
+CreateClientConVar( "flowbar", "1", true, false )
+CreateClientConVar( "flowbar_models", "crowbar stunstick", true, false )
+local flowbar = GetConVar( "flowbar" )
+local models = GetConVar( "flowbar_models" )
+
 hook.Add( "DoAnimationEvent", "Flowbar", function( ply )
+
+	if not flowbar:GetBool() then return end
 
 	local vm = ply:GetViewModel()
 	if not IsValid( vm ) then return end
-	if not string.find( vm:GetModel(), "crowbar" ) then return end
+
+	local found = false
+	for _, model in ipairs( string.Explode( " ", models:GetString() ) ) do
+		if string.find( vm:GetModel(), model ) then
+			found = true
+		end
+	end
+	if not found then return end
 
 	local seq = vm:GetSequence()
 	local act = vm:GetSequenceActivity( seq )
@@ -11,4 +25,12 @@ hook.Add( "DoAnimationEvent", "Flowbar", function( ply )
 		vm:SendViewModelMatchingSequence( miss )
 	end
 
+end )
+
+hook.Add( "PopulateToolMenu", "FlowbarSettings", function()
+	spawnmenu.AddToolMenuOption( "Options", "Player", "Flowbar_Options", "Flowbar", "", "", function( panel )
+		panel:ClearControls()
+		panel:CheckBox( "Enabled", "flowbar" )
+		panel:TextEntry( "Model List", "flowbar_models" )
+	end )
 end )
