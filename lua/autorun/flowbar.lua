652077e194f20d078b827ea28e92b409278fce69
MysteryPancake <MysteryPancake@users.noreply.github.com>
2017-10-26 16:41:00 +1100

    Added working menu

--- lua/autorun/flowbar.lua
+++ lua/autorun/flowbar.lua
@@ -1,8 +1,25 @@
+local models = { "crowbar", "stunstick" }
+for _, model in ipairs( models ) do
+	CreateClientConVar( "flowbar_" .. model, "1", true, true )
+end
+
+local function CapitalizeFirst( str )
+	return string.gsub( str, "^%l", string.upper )
+end
+
 hook.Add( "DoAnimationEvent", "Flowbar", function( ply )
 
 	local vm = ply:GetViewModel()
 	if not IsValid( vm ) then return end
-	if not string.find( vm:GetModel(), "crowbar" ) then return end
+
+	local found = false
+	for _, model in ipairs( models ) do
+		local enabled = tobool( ply:GetInfoNum( "flowbar_" .. model, 0 ) )
+		if enabled and string.find( vm:GetModel(), model ) then
+			found = true
+		end
+	end
+	if not found then return end
 
 	local seq = vm:GetSequence()
 	local act = vm:GetSequenceActivity( seq )
@@ -11,4 +28,13 @@ hook.Add( "DoAnimationEvent", "Flowbar", function( ply )
 		vm:SendViewModelMatchingSequence( miss )
 	end
 
+end )
+
+hook.Add( "PopulateToolMenu", "FlowbarSettings", function()
+	spawnmenu.AddToolMenuOption( "Options", "Player", "Flowbar_Options", "Flowbar", "", "", function( panel )
+		panel:ClearControls()
+		for _, model in ipairs( models ) do
+			panel:CheckBox( CapitalizeFirst( model ), "flowbar_" .. model )
+		end
+	end )
 end )
